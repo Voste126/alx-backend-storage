341a564d224f7767bc13a433be02b29bf99d49bf
voste126 <steveaustine126@gmail.com>
2024-10-23 17:34:52 +0000

    MongoDB

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Change school topics
+"""
+import pymongo
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update document with a specific attr: valuei
+
+    """
+    return mongo_collection.update_many({"name": name},
+            {"$set": {"topics": topics }})
